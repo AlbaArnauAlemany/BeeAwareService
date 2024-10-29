177dbdafa20e1dea7897c4272409b55c3180c7b0
onyxws <160650638+onyxws@users.noreply.github.com>
2024-10-29 17:11:47 +0100

    gitignore, dependencies pom

--- src/main/java/ch/unil/doplab/beeaware/domain/ApplicationState.java
+++ src/main/java/ch/unil/doplab/beeaware/domain/ApplicationState.java
@@ -1,4 +1,18 @@
 package ch.unil.doplab.beeaware.domain;
 
+import com.google.maps.errors.ApiException;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import static ch.unil.doplab.beeaware.Utilis.addBeezzer;
+
 public class ApplicationState  {
+
+
 }
